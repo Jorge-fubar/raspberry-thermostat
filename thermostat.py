1fd7e4c0123ae231e348175802df434ef289421d
jorge-fubar <jorgeespanadediego@gmail.com>
2019-06-02 20:43:05 +0200

    feat: added DB persistence

--- thermostat.py
+++ thermostat.py
@@ -1,11 +1,32 @@
 import os
 import glob
 import time
+import datetime
+import logging
+import sqlite3
+
+
+logging.basicConfig(level=logging.DEBUG, filename='thermostat.log',
+                    format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
+
+
+def init_db():
+    logging.info('Initializing DB...')
+    conn = sqlite3.connect('thermostat.db')
+    conn.execute('''CREATE TABLE IF NOT EXISTS temperatures
+                (date text NOT NULL, temperature real NOT NULL)''')
+    conn.execute(
+        '''CREATE INDEX IF NOT EXISTS date_index ON temperatures(date)''')
+    logging.info('DB initialized')
+    conn.commit()
+    return conn
+
 
 def read_temp_raw():
     f = open(device_file, 'r')
@@ -23,12 +44,20 @@ def read_temp():
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-    return temp_c, temp_f
+    return temp_c
+
 
 try:
+    conn = init_db()
     while True:
-        print(read_temp())
-        time.sleep(1)
+        current_time = datetime.datetime.now().strftime('%Y%m%dT%H:%M')
+        current_temp = read_temp()
+        logging.debug('Read temperature %s° at time %s',
+                      current_temp, current_time)
+        conn.execute('INSERT INTO temperatures VALUES (?, ?)',
+                     (current_time, current_temp))
+        conn.commit()
+        time.sleep(3)
 except KeyboardInterrupt:
-    print "Temperature daemon stopped by user (Ctrl+C command sent)"
+    conn.close()
+    logging.warn('Temperature daemon stopped by user (Ctrl+C command sent)')
